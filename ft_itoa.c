243df16a9bb104bb1360ddb91043c505c4fab577
root <root@DESKTOP-12PJ67V>
2024-11-07 17:10:07 -0300

    more functions to my lib

---
+++ ft_itoa.c
@@ -0,0 +1,58 @@
+	#include "libft.h"
+
+	static int	interger_len(long number)
+	{
+		int	count;
+
+		count = 0;
+		if (number < 0)
+		{
+			count++;
+			number = -number;
+		}
+		if (number == 0)
+			count++;
+		while (number != 0)
+		{
+			number /= 10;
+			count++;
+		}
+		return (count);
+	}
+
+	static char	*pre_conversion(int len)
+	{
+		char	*temporary;
+
+		temporary = (char *)malloc((len + 1) * sizeof(char));
+		if (!temporary)
+			return (NULL);
+		temporary[len] = '\0';
+		return (temporary);
+	}
+
+	char	*ft_itoa(int n)
+	{
+		int		len;
+		int		i;
+		long	number;
+		char	*result;
+
+		number = (long) n;
+		len = interger_len(number);
+		result = pre_conversion(len);
+		if (!result)
+			return (NULL);
+		i = len - 1;
+		if (number == 0)
+			result[i] = '0';
+		while (number != 0)
+		{
+			result[i] = ((number % 10) + 48);
+			number /= 10;
+			i--;
+		}
+		if (n < 0)
+			result[0] = '-';
+		return (result);
+	}
--- ft_split.c
+++ ft_split.c
@@ -1,10 +1,10 @@
 #include "libft.h"
 
-static void	ft_initiate_vars(size_t *i, int *j, int *s_word)
+static void	ft_initiate_vars(size_t *str_index, int *substr_index, int *first_word)
 {
-	*i = 0;
-	*j = 0;
-	*s_word = -1;
+	*str_index = 0;
+	*substr_index = 0;
+	*first_word = -1;
 }
 
 static void	*ft_free(char **strs, int count)
@@ -21,23 +21,23 @@ static void	*ft_free(char **strs, int count)
 	return (NULL);
 }
 
-static char	*fill_word(const char *str, int start, int end)
+static char	*Create_substr(const char *str, int start, int end)
 {
-	char	*word;
+	char	*substr;
 	int		i;
 
 	i = 0;
-	word = malloc((end - start + 1) * sizeof(char));
-	if (!word)
+	substr = malloc((end - start + 1) * sizeof(char));
+	if (!substr)
 		return (NULL);
 	while (start < end)
 	{
-		word[i] = str[start];
+		substr[i] = str[start];
 		i++;
 		start++;
 	}
-	word[i] = '\0';
-	return (word);
+	substr[i] = '\0';
+	return (substr);
 }
 
 static int	word_count(const char *str, char c)
@@ -63,28 +63,28 @@ static int	word_count(const char *str, char c)
 
 char	**ft_split(const char *s, char c)
 {
-	char	**res;
-	size_t	i;
-	int		j;
-	int		s_word;
+	char	**substring;
+	size_t	str_index;
+	int		substr_index;
+	int		first_word;
 
-	ft_initiate_vars(&i, &j, &s_word);
-	res = ft_calloc((word_count(s, c) + 1), sizeof(char *));
-	if (!res)
+	ft_initiate_vars(&str_index, &substr_index, &first_word);
+	substring = ft_calloc((word_count(s, c) + 1), sizeof(char *));
+	if (!substring)
 		return (NULL);
-	while (i <= ft_strlen(s))
+	while (str_index <= ft_strlen(s))
 	{
-		if (s[i] != c && s_word < 0)
-			s_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		if (s[str_index] != c && first_word < 0)
+			first_word = str_index;
+		else if ((s[str_index] == c || str_index == ft_strlen(s)) && first_word >= 0)
 		{
-			res[j] = fill_word(s, s_word, i);
-			if (!(res[j]))
-				return (ft_free(res, j));
-			s_word = -1;
-			j++;
+			substring[substr_index] = Create_substr(s, first_word, str_index);
+			if (!(substring[substr_index]))
+				return (ft_free(substring, substr_index));
+			first_word = -1;
+			substr_index++;
 		}
-		i++;
+		str_index++;
 	}
-	return (res);
+	return (substring);
 }
